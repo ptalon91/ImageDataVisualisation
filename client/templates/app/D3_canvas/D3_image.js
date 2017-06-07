0c49a8048f4f6db57699beaa7accb3e7b8c04f85
Patrick Talon <p.talon@bluewin.ch>
2017-06-08 00:03:48 +0200

    Premiers essais pour récupérer données pixels.
    getImageData n'a pas l'air de fonctionner sous Meteor.

--- client/templates/app/D3_canvas/D3_image.js
+++ client/templates/app/D3_canvas/D3_image.js
@@ -1,3 +1,18 @@
 // Helpers for D3Image template.
 
 // Events for D3Image template.
+
+Template.D3Image.onRendered(function() {
+	
+	let myImg = new Image();
+	myImg.src = 'image.jpg';
+
+	let c = document.getElementById("canvas");
+	let ctx = c.getContext("2d");
+	ctx.drawImage(myImg, 0, 0);
+	let img_data = ctx.getImageData(x, y, 1, 1).data;
+
+	alert(img_data)
+
+});
+

aa38dc3db0f800095ebfd8847f2287afda0d88cf
ptalon91 <p.talon@bluewin.ch>
2017-06-08 17:34:10 +0200

    Test création de fonction pour grouper valeurs de pixels

--- client/templates/app/D3_canvas/D3_image.js
+++ client/templates/app/D3_canvas/D3_image.js
@@ -2,21 +2,32 @@
 
 // Events for D3Image template.
 
+
+
 Template.D3Image.onRendered(function() {
 	
-	let myImg = new Image();
-	myImg.src = 'image.jpg';
+	//Parameters...
+	// Initialize image object.
+	let img = new Image();
+	// Image width.
+	let img_width = 300;
+	// Image height.
+	let img_height = 300;
 
-	let c = document.getElementById("canvas");
-	let ctx = c.getContext("2d");
-	ctx.drawImage(myImg, 0, 0);
-	let img_data = ctx.getImageData(0, 0, 300, 300).data;
+	// Set image object's source.
+	img.src = 'image.jpg';
+	// Get canvas from DOM and assign to variable.
+	let canvas = document.getElementById("canvas");
+	// Get canvas' context.
+	let context = canvas.getContext("2d");
+	// Draw image in the canvas.
+	context.drawImage(img, 0, 0);
+	// Get image's pixel data. Returns an array of r, g, b and a values, that are not isolated for each pixel.
+	let img_data_array = context.getImageData(0, 0, img_width, img_height).data;
 
-	console.log(img_data);
-
-	pixel_array = list();
-
-	
+	Meteor.myFunctions.group_RGBA_per_pixel(img_data_array, img_width, img_height)
 
 });
 
+
+	
